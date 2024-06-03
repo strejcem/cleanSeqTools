85dfe15e6d6a4c246de2ae6aae8bd39438bd4832
Michal Strejcek <strejcem@vscht.cz>
2024-06-03 14:23:55 +0200

    version bump

--- R/cleanSeqTools.R
+++ R/cleanSeqTools.R
@@ -1,4 +1,4 @@
-message("cleanSeqTools.R v1.32 loaded (Nov-17-2023)")
+message("cleanSeqTools.R v1.33 loaded (Nov-17-2023)")
 message("By Michal Strejcek @ UCT Prague")
 message("Depencencies: 'dada2', 'ShortRead', 'phyloseq'")
 
